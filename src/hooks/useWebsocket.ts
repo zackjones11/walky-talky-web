9560c8522f342b6c9f4b0f967b139b9ff3581d8e
Zack Jones <zackjones11@gmail.com>
2021-02-06 22:07:14 +0100

    created a useWebsocket hook, returns  sendMessage and lastMessage

---
+++ src/hooks/useWebsocket.ts
@@ -0,0 +1,51 @@
+import * as React from "react";
+
+type MessageType =
+  | "NEW_USER"
+  | "START_CHAT"
+  | "OFFER"
+  | "ANSWER"
+  | "ICE_CANDIDATE";
+
+export type WebsocketResponse = {
+  type: MessageType;
+  clientId?: number;
+  body: string;
+};
+
+interface IUseWebsocketReturn {
+  socket: WebSocket;
+  lastMessage?: WebsocketResponse;
+  sendMessage: (data: WebsocketResponse) => void;
+}
+
+const SOCKET_URL = "ws://localhost:3000/ws";
+const socket = new WebSocket(SOCKET_URL);
+
+const useWebsocket = (): IUseWebsocketReturn => {
+  const [lastMessage, setLastMessage] = React.useState<WebsocketResponse>();
+
+  socket.onopen = () => {
+    console.info("Websocket connected");
+  };
+
+  socket.onmessage = (message) => {
+    setLastMessage(JSON.parse(message.data));
+  };
+
+  socket.onclose = () => {
+    console.info("Websocket disconnected");
+  };
+
+  socket.onerror = (error) => {
+    console.error("socket error:", error);
+  };
+
+  const sendMessage = (data: WebsocketResponse) => {
+    socket.send(JSON.stringify(data));
+  };
+
+  return { socket, lastMessage, sendMessage };
+};
+
+export default useWebsocket;
