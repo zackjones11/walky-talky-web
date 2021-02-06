87201bd39c6eeb24d46176306b4ac67e6cf01317
Zack Jones <zackjones11@gmail.com>
2021-02-06 22:10:04 +0100

    init useWebsocket hook in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
+import { useWebsocket } from "./hooks";
 
-const App: React.FC = () => <div>Hello World!</div>;
+const App: React.FC = () => {
+  const { lastMessage, sendMessage } = useWebsocket();
+
+  sendMessage({ type: "NEW_USER", clientId: 1, body: "" });
+
+  return <div>{lastMessage}</div>;
+};
 
 export default App;
